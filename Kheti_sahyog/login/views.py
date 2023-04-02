587914605d52ffad2ecb5cab6bee13d6cd594873
Kumar Sanskar <85698826+Sanskar1862@users.noreply.github.com>
2023-04-02 18:05:41 +0530

    Integrated Home Page With Backend

--- Kheti_sahyog/login/views.py
+++ Kheti_sahyog/login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+
 
 def HomePage(request):
     return render(request,'home.html')
